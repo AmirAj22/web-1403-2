d83f8401423021f114234fa3f7833d6f27e096c5
AmirAj22 <amirhosein22.ajam@gmail.com>
2025-04-22 18:00:29 +0330

    Save Files

---
+++ 06/06-app-d.js
@@ -0,0 +1,121 @@
+import {writeFile, readFile} from 'fs';
+import {use, start, write} from "./05-httpFramework-f.js";
+
+use('POST', 'sum', function (request, response) {
+    response.write((parseInt(request.data.input1) + parseInt(request.data.input2)).toString());
+    response.end();
+});
+use('GET', 'sum', function (request, response) {
+    let url = request.url.split('/');
+    let inputs = url.slice(2);
+    response.write((parseInt(inputs[0]) + parseInt(inputs[1])).toString());
+    response.end();
+});
+use('GET', 'log', function (request, response) {
+    console.log('post data is:', request.data);
+    response.end();
+});
+use('GET', 'file', function (request, response) {
+    let url = request.url.split('/');
+    let inputs = url.slice(2);
+
+    readFile(inputs[0], function (error, fileBody){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, 400, 'ERROR:' + error)
+        }
+        else{
+            response.write(fileBody);
+            response.end();
+        }
+    });
+});
+use('POST', 'user', function (request, response) {
+    readFile('./users.json', 'utf8', function (error, fileData){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, 500, 'ERROR:' + error);
+        }
+        else{
+            let dataObject = JSON.parse(fileData);
+            let userCheck = false
+            dataObject.records.forEach(function(userName){
+                if(userName.user === request.data.user){
+                    userCheck = true
+                }
+                
+            })
+            dataObject.records.push(request.data);
+            let dataString = JSON.stringify(dataObject);
+            
+            if(!userCheck){
+            writeFile('./users.json', dataString, function (error){
+                if(error){
+                    console.log('ERROR:', error);
+                    write(response, 500, 'ERROR:' + error)
+                }
+                else{
+                    console.log('User Created.');
+                    write(response, 200, 'User Created.')
+                }
+            });
+            } else{
+                write(response, 400, 'User Already Exist.')
+            }
+        }
+    });
+});
+use('DELETE', 'user', function (request, response) {
+    readFile('./users.json', 'utf8', function (error, fileData){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, 500, 'ERROR:' + error);
+        }
+        else{
+            let dataObject = JSON.parse(fileData);
+            for (let i = 0; i < dataObject.records.length; i++) {
+                if(dataObject.records[i].user ===  request.data.user){
+                    console.log(dataObject.records)
+                    dataObject.records.splice(i,1)
+                }
+            }
+            console.log(dataObject)
+            dataObject.records.push(request.data);
+            let dataString = JSON.stringify(dataObject);
+            writeFile('./users.json', dataString, function (error){
+                if(error){
+                    console.log('ERROR:', error);
+                    write(response, 500, 'ERROR:' + error)
+                }
+                else{
+                    console.log('User Created.');
+                    write(response, 200, 'User Deleted.')
+                }
+            });
+        }
+    });
+});
+use('POST', 'token', function (request, response) {
+    readFile('./users.json', 'utf8', function (error, fileData){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, 500, 'ERROR:' + error);
+        }
+        else{
+            let dataObject = JSON.parse(fileData);
+            let checkUserName = false;
+            for (let i = 0; i < dataObject.records.length; i++) {
+                if(dataObject.records[i].user ===  request.data.user && dataObject.records[i].pass ===  request.data.pass){
+                    checkUserName = true
+                }
+            }
+            if(checkUserName){
+                write(response, 200, JSON.stringify(Math.random()*10000).toString())
+            } else{
+                write(response, 400, "User Or Password Wrong!!")
+            }
+        }
+    });
+});
+
+start();
